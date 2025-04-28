d19eea32f393442cd19192de119d12beea2b5f1f
Wugweb Github <132406863+wugweb-git@users.noreply.github.com>
2025-04-29 00:12:42 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "/lib/queryClient";
+import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
